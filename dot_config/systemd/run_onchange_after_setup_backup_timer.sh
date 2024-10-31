855e2d21b0c6dc114d0de45cb7052d2feefa7d77
Manuel Mueller <triantium@mailbox.org>
2024-10-31 14:49:16 +0100

    Update package list and add new scripts
    Updated the list of packages for installation on Arch Linux and added functionality for checking and installing 'mani'. Included new scripts for setting up gopass stores and backup timer hashes, and improved restic and restore scripts with more configuration checks and folders.

--- dot_config/systemd/run_onchange_after_setup_backup_timer.sh
+++ dot_config/systemd/run_onchange_after_setup_backup_timer.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# backup.service hash: {{ include "dot_config/systemd/user/backup.service" | sha256sum }}
+# backup.timer hash: {{ include "dot_config/systemd/user/backup.timer" | sha256sum }}
+
 echo 'Setting up Backup Timer'
 
 systemctl --user daemon-reload
